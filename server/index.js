d6e152946c1187fc1890e9aa91e815fc437e06ac
David <iis_dbga@live.com>
2022-12-15 15:33:38 -0600

    part 1 finished, register, login and publication validation complete

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
-app.use(cors());
-app.use(express.json())
 const User = require('./models/user.model')
 const bcrypt = require('bcryptjs')
+
+app.use(cors());
+app.use(express.json())
 
 mongoose.set('strictQuery',false);
 mongoose.connect('mongodb://localhost:27017/full-mern-test')
@@ -31,40 +32,41 @@ app.post('/api/register', async (req,res)=>{
             email: req.body.email,
         });
             
-        if(!user){ return { status: 'error', error:'Invalid Login'} }
+        if(!user){ 
+            return { status: 'error', error:'Invalid Login'} }
+
         const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
 
-if(isPasswordValid){
+        if(isPasswordValid){
 
-    const token = jwt.
-    sign({
-        name: user.name,
-        email: req.body.email
-    },"secret123")
+            const token = jwt.sign({
+                name: user.name,
+                email: req.body.email
+            },"secret123")
 
-return res.json({ status:'ok', user:token})
-    }else{
-return res.json({ status: 'error', user:false})
+    return res.json({ status:'ok', user:token})
+        }else{
+    return res.json({ status: 'error', user:false})
     }
-    }
-)
+    })
 
-app.get('/api/quote', async (req,res)=>{
-    const token = req.header['x-access-token'];
-    try{
-    const decoded = jwt.verify(token,'secret123')
-    const email = decoded.email
-    const user = await User.findOne({email:email})
-        return res.json({ status: 'ok', quote:user.quote})
-    }
-    catch(error){
-    console.log(error)
-    res.json({ status:'error', error:'invalid token'})
+        app.get('/api/quote', async (req,res)=>{
+            const token = req.headers['x-access-token'];
+            try{
+            const decoded = jwt.verify(token,'secret123')
+            const email = decoded.email
+            const user = await User.findOne({email:email})
+            return res.json({ status: 'ok', quote:user.quote})
+            }
+        catch(error){
+        console.log(error)
+        res.json({ status:'error', error:'invalid token'})
     }
 })
 
 app.post('/api/quote', async (req,res)=>{
-    const token = req.header['x-access-token'];
+    const token = req.headers['x-access-token'];
+    
     try{
     const decoded = jwt.verify(token,'secret123')
     const email = decoded.email
